950171136d9c3ff680342c39b766a528c174cabe
Michael Phillips <mlp.brooklyn@gmail.com>
2013-08-15 16:23:09 -0700

    Update helpers.js

--- helpers.js
+++ helpers.js
@@ -1,1 +1,19 @@
 // helpers.js
+
+// 
+// global data-js query
+// $.js('foo');
+
+jQuery.js = function(jsSelector, scope) {
+    var scopeSelector = (scope && scope instanceof jQuery) ? scope.selector : 'html'
+
+    return jQuery(scopeSelector + ' [data-js~="' + jsSelector + '"]');
+}
+
+// 
+// scoped data-js query
+// $.js('foo').js('bar');
+
+jQuery.fn.js = function(jsSelector) {
+    return $.js(jsSelector, this);
+}
